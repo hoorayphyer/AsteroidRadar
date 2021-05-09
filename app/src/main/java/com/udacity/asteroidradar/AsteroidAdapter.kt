8edbfd247b25574bf072dbe29bdd18d60e7c1376
hooray <hrfudan@gmail.com>
2021-05-09 16:22:19 -0400

    implemented ViewHolder using data binding

--- app/src/main/java/com/udacity/asteroidradar/AsteroidAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidAdapter.kt
@@ -1,5 +1,6 @@
 package com.udacity.asteroidradar
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -9,6 +10,25 @@ import com.udacity.asteroidradar.databinding.ListItemAsteroidBinding
 class AsteroidAdapter : ListAdapter<Asteroid, AsteroidAdapter.ViewHolder,>(AsteroidAdapter.DiffCallback()) {
 
     class ViewHolder private constructor( val binding : ListItemAsteroidBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Asteroid) {
+            binding.asteroidSerialNumber.text = item.id.toString()
+            binding.asteroidCloseApproachDate.text = item.closeApproachDate
+            binding.imageView.setImageResource(
+                    when(item.isPotentiallyHazardous) {
+                        true -> R.drawable.asteroid_hazardous
+                        else -> R.drawable.asteroid_safe
+                    }
+            )
+        }
+
+        companion object {
+            fun from(parent: ViewGroup) : ViewHolder {
+                val inflater =LayoutInflater.from(parent.context)
+                val binding = ListItemAsteroidBinding.inflate(inflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
 
     }
 
@@ -23,10 +43,11 @@ class AsteroidAdapter : ListAdapter<Asteroid, AsteroidAdapter.ViewHolder,>(Aster
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        holder.bind(item)
     }
 }
